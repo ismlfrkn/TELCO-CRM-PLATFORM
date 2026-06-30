c7ea724b40441436c147fe477825ba3bfb24b593
ismlfrkn <bilenismailfurkan@gmail.com>
2026-06-30 16:57:45 +0300

    feat(gateway): add api gateway service and config

---
+++ api-gateway/src/test/java/com/turkcell/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
